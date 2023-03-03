c653b84142895c2273303e6f7a4fec0baf83388d
antonio pino <antonio.pino@iisgubbio.edu.it>
2023-03-03 01:03:34 +0100

    s

--- oii/latest/main.java
+++ oii/latest/main.java
@@ -1,130 +1,146 @@
 import java.util.*;
 import java.io.*;
 
+class Main {
 
-/*TODO:
-
-rifai tutta la parte bassa ma senza usare un array per gli archi, ma invece usa l'oggetto stesso
-
-*/
-
-
-
-public class main {
-
-    // uso un oggeto dato che il grafico è semplice(un nomo massimo ha 4 archi)
-    // rispetto ad una matrice che sì fa risparmiare tempo ma è non avendo nodi con moltissimi archi
-    // mi bastano gli oggetti che sono più intuitivi e semplici da gestire
-    // ============== classi nodo e arco ==============
-    static class Node{
+    // ------------------ INIZIO implementazione dell'oggetto Grafo ---------
+    static class Nodo implements Comparable<Nodo>{
         int id;
-        ArrayList<Edge> edges = new ArrayList<>();
+        int costoPercorso;
+        int bigger;
+        Nodo padre;
+        ArrayList<Arco> archi;
+        public Nodo( int id ){
+            this.id = id;
+            archi = new ArrayList<>();
+        }
+        public String toString(){
+            String vicini = "";
+            for(Arco a: archi){
+                vicini+=a;
+            }
+            return "{"+id+" $:"+costoPercorso+" ^"+(padre==null?"-":padre.id)+"} "+vicini;
+        }
+        @Override
+        public int compareTo(Nodo o) {
+            return this.costoPercorso - o.costoPercorso;
+        }
+    }
+    static class Arco{
+        Nodo a;
+        int peso;
+        // il costruttore di un arco inserisce anche l'arco stesso
+        // nel nodo relativo, il parametro "da" viene ignorato
+        // in questa implementazione
+        public Arco(int da, int a, int peso){
+            this.a = grafo[a];
+            this.peso = peso;
+            grafo[da].archi.add(this);
+        }
+        public String toString(){
+            return "[-> "+a.id+" $"+peso+"]";
+        }
+    }
+    static Nodo grafo[];
+    // ------------------ FINE implementazione dell'oggetto Grafo -----------
 
-        public Node(int i){
-            id = i;
+    // ------ stampe di utilità (in pratica solo robe decorative) -----------
+    static void dump(){
+        for(Nodo n:grafo){
+            System.out.println(n);
+        }
+    }
+    static void coperturaDump(){
+        System.out.print("P");
+        Nodo n=null;
+        for(int i=0; i<grafo.length; i++){
+            if(grafo[i].padre==null){
+                n = grafo[i];
+                break;
+            }
+        }
+        coperturaDumpRec(0, n);
+    }
+    private static void coperturaDumpRec(int nest, Nodo n){
+        String indent = new String(new char[nest*2]).replace('\0', ' ');
+        System.out.println(indent+n);
+        for(int i=0; i<grafo.length; i++){
+            if(grafo[i].padre == n){
+                coperturaDumpRec(nest+1, grafo[i]);
+            }
         }
     }
 
-    static class Edge{
-        Node a, b;
-        int weight = 0;
-        public Edge(Node a, Node b, int weight){
-            this.a = a;
-            this.b = b;
-            this.weight = weight;
+    // ---------- Dijkstra ----------
+    static void camminiMinimi(Nodo x){
+        // pulizia
+        for(Nodo n: grafo){
+            n.costoPercorso = Integer.MAX_VALUE;
+            n.padre = null;
+            n.bigger = 0;
+        }
+
+        // costruzione partenza
+        x.costoPercorso = 0;
+        PriorityQueue<Nodo> s = new PriorityQueue<>();
+        s.add(x);
+        
+        // costruzione della copertura
+        while( !s.isEmpty() ){
+            Nodo attuale = s.poll();
+            for(Arco a: attuale.archi){
+                Nodo vicino = a.a;
+                int nDist = attuale.costoPercorso + a.peso;
+                if( nDist < vicino.costoPercorso ){
+                    vicino.costoPercorso = nDist;
+                    vicino.padre = attuale;
+                    s.add(vicino);
+                    if(a.peso > attuale.bigger) {a.a.bigger = a.peso;} else {a.a.bigger = attuale.bigger;}
+                }
+            }
         }
     }
 
-    // ============== metodo risoluzione ==============
-    public int solve(int H, int W, Node[] nodes, Edge[] edges) {
-
-        graph(H, W, nodes);
+    public int solve(){
         return 0;
     }
-    
-    public void graph(int H, int W, Node[] nodes){
-        for (int i = 0; i < H*W; i++) {
-            System.out.printf(" %d    %d    ", i, nodes[i].ed weight);
-        }
-    }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        // se preferisci leggere e scrivere da file
-        // ti basta modificare la seguente variabile
-        boolean input_from_file = true;
-
-        InputStream fin;
-        OutputStream fout;
-        if(input_from_file) {
-            fin = new FileInputStream("input.txt");
-            // fin = new FileInputStream("abc_quadri.input33.txt");
-            fout = new FileOutputStream("output.txt");
-        } else {
-            fin = System.in;
-            fout = System.out;
-        }
-
+    public static void main(String[] args) throws Exception{
+        InputStream fin = new FileInputStream("input.txt");
+        OutputStream fout = new FileOutputStream("output.txt");
         Scanner scn = new Scanner(fin);
         PrintStream prnt = new PrintStream(fout);
         int T = scn.nextInt();
-        for(int t = 1; t <= T; t++) {
-            int H = scn.nextInt();
-            int W = scn.nextInt();
 
-            Node nodes[] = new Node[H*W];
-            Edge edges[] = new Edge[(H-1)*W+(W-1)*H];
-            int id = 0;
+        for (int t = 1; t <= T; t++) {
+            
+            int R = scn.nextInt();
+            int C = scn.nextInt();
+            int numeroNodi = R*C;
+            grafo = new Nodo[numeroNodi];
+            int altitudini[] = new int[numeroNodi];
+            for(int i=0; i<numeroNodi; i++){
+                grafo[i] = new Nodo(i);
+                altitudini[i] = scn.nextInt();
+            }
 
-            int[][] S = new int[H][W];
-            for (int h = 0; h < H; h++) {
-                for (int w = 0; w < W; w++) {
-                    S[h][w] = scn.nextInt();
-                    nodes[id] = new Node(id++);
+            //int numeroArchi = (R-1)*C+(C-1)*R;
+            for (int h = 0, i = 0; h < R; h++) {
+                for (int w = 0; w < C; w++, i++) {
+                    if(w!=0){   new Arco( i, i-1, Math.abs(altitudini[i]-altitudini[i-1]) ); } // LEFT
+                    if(h!=0){   new Arco( i, i-C, Math.abs(altitudini[i]-altitudini[i-C]) ); } // TOP
+                    if(w!=C-1){ new Arco( i, i+1, Math.abs(altitudini[i]-altitudini[i+1]) ); } // RIGHT
+                    if(h!=R-1){ new Arco( i, i+C, Math.abs(altitudini[i]-altitudini[i+C]) ); } // BOTTOM
                 }
             }
 
-            id = 0;
-            int idNodo = 0;
-            int jump = 0;
-            for (int h = 0; h < H; h++) {
-                for (int w = 0; w < W; w++) {
-                    if(w!=W-1){
-                        edges[id++] = (new Edge(nodes[idNodo], 
-                        nodes[idNodo+1],
-                        Math.abs(S[h][w]-S[h][w+1]))); 
-                    } // RIGHT
-                    if(h!=H-1){ 
-                        edges[id++] = (new Edge(nodes[idNodo],
-                        nodes[idNodo+jump], 
-                        Math.abs(S[h][w]-S[h+1][w])));
-                    } // BOTTOM
-                }
-                jump+=W;
-            }
-            main solver = new main();
-            int risposta = solver.solve(H, W, nodes, edges);
-
-
-            prnt.format("Case #%d: %d\n", t, risposta);
+            camminiMinimi(grafo[0]);
+            coperturaDump();
+            System.out.println("===========");
+            prnt.format("Case #%d: %s\n", t, ""+grafo[numeroNodi-1].bigger);
             fout.flush();
-
-            System.out.println("======================");
         }
-
-        prnt.close();
         scn.close();
+        fin.close();
     }
 }
-
-
-/*
-
-The choice of representation for a graph depends on the problem at hand and the operations that need to be performed on the graph.
-
-Using an object-oriented representation, as I did in the first request, can be more flexible and easier to work with when dealing with complex graphs with variable node degrees and edge weights. This approach can also be more intuitive, as each node and edge is represented as an object with its own properties and methods.
-
-On the other hand, using a matrix representation, as in the second request, can be more efficient for certain types of algorithms that require fast lookup of edge weights. It can also be more space-efficient for dense graphs, where most nodes are connected to each other.
-
-In general, if the graph is small and dense, a matrix representation may be more appropriate. If the graph is large and sparse, an object-oriented representation may be more suitable. However, it is always a good idea to consider the specifics of the problem at hand and choose the representation that best fits the requirements of the algorithm or application.
-
-*/
